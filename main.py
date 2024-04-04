24db971ecc5b9bfcd22dca6ac92d843e292eddb3
Saikal Ulukbekova <ukyzsaikal@gmail.com>
2024-04-04 18:22:16 +0200

    Snake eats an apple and Added score

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import pygame
 from pygame.locals import *
 import time
+import random
 
 SIZE = 40
 
@@ -17,6 +18,10 @@ class Apple:
         self.parent_screen.blit(self.image, (self.x, self.y))
         pygame.display.flip()
 
+    def move(self):
+        self.x = random.randint(0,24) * SIZE
+        self.y = random.randint(0, 19) * SIZE
+
 class Snake:
     def __init__(self, parent_screen , length):
         self.length = length
@@ -25,6 +30,12 @@ class Snake:
         self.x = [SIZE]*length
         self.y = [SIZE ]*length
         self.direction = 'down'
+
+    def increase_length(self):
+        self.length += 1
+        self.x.append(-1)
+        self.y.append(-1)
+
 
     def move_left(self):
         self.direction = 'left'
@@ -74,6 +85,24 @@ class Game:
     def play(self):
         self.snake.walk()
         self.apple.draw()
+        self.score()
+        pygame.display.flip()
+
+        if self.is_collision(self.snake.x[0], self.snake.y[0], self.apple.x, self.apple.y):
+            self.snake.increase_length()
+            self.apple.move()
+
+    def score(self):
+        font = pygame.font.SysFont('arial', 30)
+        score = font.render(f'Score: {self.snake.length}', True, (255, 255, 255))
+        self.surface.blit(score, (800, 10))
+
+    def is_collision(self, x1, y1, x2, y2):
+        if x1 >= x2 and x1 < x2 + SIZE:
+            if y1 >=   y2 and y1 < y2 + SIZE:
+                 return True
+        return False
+
 
     def run(self):
         running = True
